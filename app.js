f669939d05bde4ac5c2c5a9faad906ee26c7a134
Joseph Francis <joefran@us.ibm.com>
2019-04-04 08:32:25 -0400

    Preview available from apps directly

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ function onListening(server) {
 preview.use(bodyParser.json());
 preview.use(bodyParser.urlencoded({ extended: false }));
 preview.use(cookieParser());
-preview.use(express.static(scope.locals.path.root + '/local_preview'));
+preview.use(express.static(scope.locals.path.root + '/local_ws'));
 
 // error handlers
 preview.use(function (req, res, next) {
