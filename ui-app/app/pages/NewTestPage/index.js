090d12727f895ea7472e7eba2686d938deca5503
Joseph Francis <joefran@us.ibm.com>
2019-04-04 14:35:12 -0400

    Test Page

--- ui-app/app/pages/NewTestPage/index.js
+++ ui-app/app/pages/NewTestPage/index.js
@@ -99,17 +99,32 @@ License: MIT
     //===== PAGE-ACTIONS-START
     actions.loadASpot = loadASpot;
     function loadASpot(){
-        ThisPage.loadSpot("funspot", "We are having fun now")
+        var tmpHTML = [];
+        tmpHTML.push('<div class="ui-layout-center">Center')
+        tmpHTML.push('</div>')
+        tmpHTML.push('<div class="ui-layout-north">North</div>')
+        tmpHTML.push('<div class="ui-layout-south">South</div>')
+        tmpHTML.push('<div class="ui-layout-east">East</div>')
+        tmpHTML.push('<div class="ui-layout-west">West</div>')
+        tmpHTML = tmpHTML.join('');
+
+        ThisPage.loadSpot("body",tmpHTML);
+        var tmpBodySpot =  ThisPage.getSpot("body");
+        var tmpLayout = tmpBodySpot.layout();
+        console.log( 'tmpLayout', tmpLayout);
+        if (typeof (ThisApp.refreshLayouts) == 'function') {
+            ThisApp.refreshLayouts();
+        }
+        console.log( 'tmpBodySpot', tmpBodySpot);
+
+        
     };
     //===== PAGE-ACTIONS-END
 
     
-    //===== CUSTOM-PAGE-CODE-START
     //===== NO EDIT ABOVE THIS LINE ====== ====== ====== ====== ====== ====== ====== ====== 
-    actions.loadASpot = loadASpot;
-    function loadASpot(){
-        ThisPage.loadSpot("funspot", "We are having fun now")
-    };
+    //===== CUSTOM-PAGE-CODE-START
+    
 
     //===== CUSTOM-PAGE-CODE-END
 })(ActionAppCore, $);
