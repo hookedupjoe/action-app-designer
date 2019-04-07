19b3b7af12d1449e13128a57e4ab0619ae57af13
Joseph Francis <joefran@us.ibm.com>
2019-04-07 08:51:25 -0400

    New Control - dropmenu with contents

--- ui-app/app/pages/AppBuilderPage/controls/center/control.js
+++ ui-app/app/pages/AppBuilderPage/controls/center/control.js
@@ -5,17 +5,44 @@ License: MIT
 (function (ActionAppCore, $) {
 
 	var ControlSpecs = {
-		"options" : {
-			padding: false
+		"options": {
+			padding: true
 		},
 		"content": [
-			
-		]	
-	
+			{
+				ctl: "title",
+				size: "large",
+				text: "Testing Drop Down Menu"
+			},
+			{
+				ctl: "dropmenu",
+				icon: "sidebar",
+				text: "Select Person",
+				content: [
+					{
+						ctl: "item",
+						classes: 'fluid',
+						attr: {
+							pageaction: 'runTest1'
+						},
+						text: "Run Test 1"
+					},
+					{
+						ctl: "item",						
+						classes: 'fluid',
+						attr: {
+							pageaction: 'runTest2'
+						},
+						text: "Run Test 2"
+					}
+				]
+			}
+		]
+
 	}
 
 	var ControlCode = {};
-	var ThisControl = {specs: ControlSpecs, options: { proto: ControlCode, parent: ThisApp }};
+	var ThisControl = { specs: ControlSpecs, options: { proto: ControlCode, parent: ThisApp } };
 
 	return ThisControl;
 
