852ea1496d74d493853e950ad2090f6c04b8f38d
Joseph Francis <hookedupjoe@gmail.com>
2023-06-27 21:29:01 -0400

    new endpoint to get app docs from Mongo

---
+++ designer-server/appdata/api/get-appdocs.js
@@ -0,0 +1,86 @@
+'use strict';
+const THIS_MODULE_NAME = 'get-appdocs';
+const THIS_MODULE_TITLE = 'Data: Get Application Docs from MongoDB';
+//ToDo: Save and Create as one .. just save?  
+//      Add a flag for create?
+module.exports.setup = function setup(scope) {
+    var config = scope;
+    var $ = config.locals.$;
+    
+    function Route() {
+        this.name = THIS_MODULE_NAME;
+        this.title = THIS_MODULE_TITLE;
+    }
+    var base = Route.prototype;
+
+    var $ = config.locals.$;
+
+    //--- Load the prototype
+    base.run = async function (req, res, next) {
+        var self = this;
+        return new Promise( async function (resolve, reject) {
+            try {
+                var tmpBody = req.body || {};
+                if (typeof (tmpBody) == 'string') {
+                    try {
+                        tmpBody = JSON.parse(tmpBody)
+                    } catch (ex) {
+                        throw("Bad JSON Passed")
+                    }
+                }
+                console.log('tmpBody',tmpBody);
+                var tmpTest = {
+                    accountid: 'local',
+                    dbname: 'actapp-demoapp1',
+                    doctype: 'person'
+                };
+                
+                var tmpAccount = await $.MongoManager.getAccount(tmpBody.accountid);
+                var tmpDB = await tmpAccount.getDatabase(tmpBody.dbname);
+                var tmpDocType = tmpBody.doctype;
+                var tmpMongoDB = tmpDB.getMongoDB();
+                console.log('tmpMongoDB',tmpMongoDB);
+                var tmpDocs = await tmpMongoDB.collection('actapp-' + tmpDocType).find().toArray();
+                var tmpRet = {success:true};
+                
+                console.log('tmpDocs',tmpDocs);
+                tmpRet = $.merge(false, tmpRet, {data:tmpDocs});
+
+                resolve(tmpRet);
+
+            }
+            catch (error) {
+                console.log('Err : ' + error);
+                reject(error);
+            }
+
+        });
+
+
+
+    }
+
+
+
+
+
+    //====== IMPORTANT --- --- --- --- --- --- --- --- --- --- 
+    //====== End of Module / setup ==== Nothing new below this
+    return  async function processReq(req, res, next) {
+        try {
+            var tmpRoute = new Route();
+            var tmpResults = await(tmpRoute.run(req, res, next));
+
+            //--- Getting documents to use directly by source, 
+            //    .. do not wrap the success flag
+            res.json(tmpResults)
+        } catch (ex) {
+            res.json({ status: false, error: ex.toString() })
+        }
+    }
+};
+
+
+
+
+
